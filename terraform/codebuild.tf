6374974c80757ddc511fc88ca14d5069a900a5d6
Devraj <devrajmandal5650@gmail.com>
2025-07-28 04:00:56 +0530

    Major fixes

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -1,6 +1,8 @@
-resource "aws_codebuild_project" "react_build" {
-  name         = "ReactAppBuild"
-  service_role = aws_iam_role.codebuild_role.arn
+resource "aws_codebuild_project" "build_project" {
+  name          = "ReactAppBuild"
+  description   = "CodeBuild project for building and pushing Docker image"
+  service_role  = aws_iam_role.codebuild_role.arn
+  build_timeout = 30 # Minutes
 
   artifacts {
     type = "CODEPIPELINE"
@@ -10,9 +12,8 @@ resource "aws_codebuild_project" "react_build" {
     compute_type    = "BUILD_GENERAL1_SMALL"
     image           = "aws/codebuild/standard:6.0"
     type            = "LINUX_CONTAINER"
-    privileged_mode = true
+    privileged_mode = true # Required for Docker builds
 
-    # Environment variables for Docker image build and push
     environment_variable {
       name  = "AWS_ACCOUNT_ID"
       value = var.aws_account_id
@@ -27,11 +28,16 @@ resource "aws_codebuild_project" "react_build" {
       name  = "IMAGE_REPO_NAME"
       value = var.image_repo_name
     }
+
+    environment_variable {
+      name  = "IMAGE_TAG"
+      value = "latest"
+    }
   }
 
   source {
     type      = "CODEPIPELINE"
-    buildspec = "buildspec.yml" # Correct placement
+    buildspec = "buildspec.yml" # Ensure this file is in root of your repo
   }
 
   logs_config {
