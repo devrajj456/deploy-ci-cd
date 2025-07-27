1bec5f6b02f171e0d90ebb7a2627a3cc43c99b6b
Devraj <devrajmandal5650@gmail.com>
2025-07-28 03:00:00 +0530

    Minor fixes

--- terraform/codedeploy.tf
+++ terraform/codedeploy.tf
@@ -16,28 +16,12 @@ resource "aws_codedeploy_deployment_group" "deployment_group" {
   service_role_arn      = aws_iam_role.codedeploy_role.arn
 
   # Deployment configuration
-  deployment_config_name = "CodeDeployDefault.AllInstancesAtOnce"
+  deployment_config_name = "CodeDeployDefault.AllInstancesAtOnceServer"
 
   # Auto Rollback configuration
   auto_rollback_configuration {
     enabled = true
     events  = ["DEPLOYMENT_FAILURE"]
-  }
-
-  # Blue/Green deployment configuration (optional)
-  blue_green_deployment_config {
-    terminate_blue_instances_on_deployment_success {
-      action                         = "TERMINATE"
-      termination_wait_time_in_minutes = 5
-    }
-
-    deployment_ready_option {
-      action_on_timeout = "CONTINUE_DEPLOYMENT"
-    }
-
-    green_fleet_provisioning_option {
-      action = "COPY_AUTO_SCALING_GROUP"
-    }
   }
 
   # Target instances using Auto Scaling Group
